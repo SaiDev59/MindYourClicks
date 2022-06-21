3a50d5128c22b63b941d5c8e6341ab1569b0f978
Sai Dev M V <saidevmv@Sais-MacBook-Pro.local>
2022-06-21 17:26:53 +0530

    Redundant code has been removed with new function declaration

--- code.js
+++ code.js
@@ -1,197 +1,182 @@
-//in this approach of reshuffling i am shuffling the images where grid are staying at the same place
-//create an array of img
-let startBtn = document.querySelector('.start');
-let mainGrid = document.querySelector('section #container')
-let displayTime = document.querySelector('.displayTime')
+// VARIABLES----------------------------------
 let images = []
-//the below code is done for getting new set of images in a game for a better experience
-let j =Math.floor(Math.random()*74)+10
-for(let i=0;i<16;i++,j++)
-{
+const gridLength = 16
+let isClicked = new Array(gridLength).fill(false)
+let currScore = 0;
+let highScore = 0;
+let countDown = null;
+let secs = 60;
+
+const startBtn = document.querySelector('.start');
+const mainGrid = document.querySelector('section #container')
+const displayTime = document.querySelector('.displayTime')
+
+const gameGrids = document.querySelectorAll('#container div')
+const hScore = document.querySelector('#highscorenumber')
+const hName = document.querySelector('#highscorename')
+const cScore = document.querySelector('#currscorenumber')
+const cName = document.querySelector('#currscorename')
+
+const pauseBtn = document.querySelector('.pause');
+const resetBtn = document.querySelector('.reset');
+
+
+//EVENT LISTENERS----------------------------
+
+function divsEventListeners(index) {
+    index.addEventListener('click', mainMeat);
+}
+startBtn.addEventListener('click', start);
+pauseBtn.addEventListener('click', pause);
+resetBtn.addEventListener('click', reset);
+
+// FUNCTIONS-----------------------------------
+//The below code is done for getting new set of images in a game for a better experience
+
+const clockResetting = () => {
+    clearInterval(countDown);
+    countDown = null;
+    secs = 60;
+    displayTime.innerHTML = secs;
+}
+
+const bindingEvents = (first, second) => {
+    alert(`final score is ${first} and Max score is ${second}`);
+    rearrange(images);
+    reshuffleImages();
+    displayScore();
+    currScore = 0;
+    hideGame();
+}
+
+gameGrids.forEach(divsEventListeners);
+
+let j = Math.floor(Math.random() * 74) + 10
+
+for (let i = 0; i < 16; i++, j++) {
     images[i] = `<img id=${i} src= "Pictures/00${j}.png" width=100% height=100%>`
 }
-const divs = document.querySelectorAll('section  section  div')
 
-startBtn.addEventListener('click',()=>{
-    mainGrid.classList.add("grid-container");
-    mainGrid.classList.remove("gridBlur");
-    showGame();
-    
-})
-    let hScore = document.querySelector('#highscorenumber')
-    let hName = document.querySelector('#highscorename')
-    let cScore = document.querySelector('#currscorenumber')
-    let cName = document.querySelector('#currscorename')
-
-
-function rearrange(inputArray){
+function rearrange(inputArray) {
     array2 = new Array();
-    var counter = 0;
-    let looplength = inputArray.length;
-    for(var i = 0;i<looplength;i++)
-        {
-            let index = Math.floor(Math.random()*inputArray.length);
-            array2[counter] = inputArray[index];
-            inputArray.splice(index,1)
-            counter++;
-        }
-        //filling the emptied inputarray for later use
-        for(var i of array2)
-            inputArray.push(i)
+    let counter = 0;
+    const looplength = inputArray.length;
+    for (let i = 0; i < looplength; i++) {
+        let index = Math.floor(Math.random() * inputArray.length);
+        array2[counter] = inputArray[index];
+        inputArray.splice(index, 1)
+        counter++;
     }
+    //Filling the emptied inputarray for later use
+    for (let i of array2)
+        inputArray.push(i)
+}
 
-    let isClicked = new Array(16).fill(false)
-    let currScore = 0;
-    let highScore = 0;
 
-function reshuffleImages(){
-    for(let i=0;i<16;i++)
-    {
-        divs[i].innerHTML = images[i];
+
+const reshuffleImages = () => {
+    for (let i = 0; i < 16; i++) {
+        gameGrids[i].innerHTML = images[i]; // Images have tags, so innerHTML has to be used.
     }
 }
-function showGame(){
-    for(let i = 0;i<16;i++)
-    {
-        
-        divs[i].classList.add("grid")
-        divs[i].innerHTML = images[i];
-        
+const showGame = () => {
+    for (let i = 0; i < 16; i++) {
+        gameGrids[i].classList.add("grid")
+        gameGrids[i].innerHTML = images[i];
     }
 }
-function hideGame(){
-    mainGrid.classList.remove("grid-container")
-        for(let i = 0;i<16;i++)
-        {
-            
-            divs[i].classList.remove("grid");
-            divs[i].innerHTML = "";
-            
-        }
+function removing(index) {
+    index.classList.remove("grid");
+    index.innerHTML = "";
 }
-function mainMeat()
-{
-    //below line gets the id of image in the div
+const hideGame = () => {
+    mainGrid.classList.remove("grid-container");
+    gameGrids.forEach(removing);
+}
+function mainMeat() {
+    //Below line gets the id of image in the div
     let index = this.firstElementChild.id;
-    //if u click all the 16 unique then only will this if condition will be executed
-    if(currScore==15 && isClicked[index]==false)
-    {
-        highScore = currScore+1;
-        currScore+=1;//this is far that displaying score thing inside the function displayRecord()
-        alert(`final score is ${highScore} and Max score is ${highScore}`)
+    //If u click all the 16 unique then only will this if condition will be executed
+    if (currScore === 15 && isClicked[index] === false) {
+        highScore = currScore + 1;
+        currScore += 1;//This is far that displaying score thing inside the function displayRecord()
+        bindingEvents(highScore, highScore);
+        clockResetting();
         isClicked.fill(false)
-        rearrange(images)
-        reshuffleImages();
-        clearInterval(countDown);
-        countDown = null;
-        secs = 60;
-        displayTime.innerHTML = secs;
-        displayScore();
-        currScore = 0;
-        hideGame();
-
         return;
 
     }
-    //if u dont click all the 16 and end your game in between
-    if(isClicked[index])
-    {
-        clearInterval(countDown);
-        countDown = null;
-        secs = 60;
-        displayTime.innerHTML = secs;
-        alert(`final score is ${currScore} and Max score is ${highScore}`)
-        //reset the game here
-
+    //If u dont click all the 16 and end your game in between
+    if (isClicked[index]) {
+        clockResetting();
+        bindingEvents(currScore, highScore);
+        //Reset the game here
         isClicked.fill(false);
-        rearrange(images)
-        reshuffleImages();
-        displayScore();
-        currScore = 0;
-        hideGame();
         return;
 
     }
     currScore++;
-    highScore = Math.max(highScore,currScore)
+    highScore = Math.max(highScore, currScore)
     isClicked[index] = true;
     rearrange(images);
     reshuffleImages();
 }
 
-
-
-//adding event to each grid
-for(let i=0;i<16;i++)
-divs[i].addEventListener('click',mainMeat)
-
-
-function displayScore(){
+const displayScore = () => {
     const name = prompt("Whats your name player?");
-    hScore.innerHTML = highScore;
-    cScore.innerHTML = currScore
-    
-    if(hScore.innerHTML=="")
-    {
-        console.log("high=0")
-        hScore.innerHTML = highScore;
-        hName.innerHTML = name;
-        cScore.innerHTML = currScore;
-        cName.innerHTML = name;
+    hScore.textContent = highScore;
+    cScore.textContent = currScore;
+
+    // !hScore.innerHTML.length
+
+    if (hScore.innerHTML === "") {
+        scoreUpdate(name);
+
     }
-    else if(parseInt(hScore.innerHTML)!=0)
-    {
-        if(parseInt(cScore.innerHTML)>=parseInt(hScore.innerHTML))
-        {
-            console.log("cur>high")
-        hScore.innerHTML = highScore;
-        hName.innerHTML = name;
-        cScore.innerHTML = currScore;
-        cName.innerHTML = name;
+    else if (parseInt(hScore.innerHTML)) {
+        if (parseInt(cScore.innerHTML) >= parseInt(hScore.innerHTML)) {
+            scoreUpdate(name);
         }
-        else if(parseInt(cScore.innerHTML)<parseInt(hScore.innerHTML))
-        {
-            console.log("curr<high")
+        else if (parseInt(cScore.innerHTML) < parseInt(hScore.innerHTML)) {
             cScore.innerHTML = currScore;
             cName.innerHTML = name;
 
         }
     }
-    else{
+    else {
         hName.innerHTML = name;
         cName.innerHTML = name;
     }
 }
-// buttons logic
 
-let pauseBtn = document.querySelector('.pause');
-let resetBtn = document.querySelector('.reset');
-let countDown = null;
-let secs = 60;
+function scoreUpdate(name) {
+    hScore.innerHTML = highScore;
+    hName.innerHTML = name;
+    cScore.innerHTML = currScore;
+    cName.innerHTML = name;
+}
 
-startBtn.addEventListener('click', start);
-pauseBtn.addEventListener('click', pause);
-resetBtn.addEventListener('click', reset);
+const removeHover = (index) => {
+    index.classList.remove("noHover");
+}
 
 function start() {
-    //if its resume then the b`elow button will change this to start
-    
+    //If its resume then the b`elow button will change this to start.
     document.querySelector('.start').innerHTML = "Start";
-    //below 2 line code is so that after game is paused i shouldnt see the hover effect on game tiles
-    for(let i=0;i<16;i++)
-    divs[i].classList.remove("noHover")
-    if(countDown) {
+    mainGrid.classList.add("grid-container");
+    mainGrid.classList.remove("gridBlur");
+    showGame();
+    //Below 2 line code is so that after game is paused i shouldnt see the hover effect on game tiles
+    gameGrids.forEach(removeHover)
+    if (countDown) {
         alert("the game is already Running!!")
         return
     }
-    countDown = setInterval(function() {
+    countDown = setInterval(function () {
 
         secs--;
         if (secs === 0) {
-            displayTime.innerHTML = "0" + secs;
-            clearInterval(countDown);
-            secs = 60;
-            countDown = null; 
+            clockResetting();
             alert(`final score is ${currScore} and Max score is ${highScore}`)
             isClicked.fill(false);
             displayScore();
@@ -199,45 +184,40 @@ function start() {
             hideGame();
 
         }
-        if (secs<10 && secs > 0)
+        if (secs < 10 && secs > 0)
             displayTime.innerHTML = "0" + secs;
         else displayTime.innerHTML = secs;
     }, 1000)
 }
 
-function pause () {
-    if(!countDown)
-    {
+function pause() {
+    if (!countDown) {
         alert("the game is not started")
         return
     }
     mainGrid.classList.add("gridBlur");
-    //below 2 line code is so that after game is paused i shouldnt see the hover effect on game tiles
-    for(let i=0;i<16;i++)
-    divs[i].classList.add("noHover")
+    //Below 2 line code is so that after game is paused i shouldnt see the hover effect on game tiles
+    for (let i = 0; i < 16; i++)
+        gameGrids[i].classList.add("noHover")
     document.querySelector('.start').innerHTML = "Resume";
     clearInterval(countDown);
-    countDown=null;
+    countDown = null;
 
 }
 
-function reset () {
-    if(!countDown && document.querySelector('.start').innerHTML != "Resume") {
+function reset() {
+    if (!countDown && document.querySelector('.start').innerHTML != "Resume") {
         return;
     }
-    let toConfirm = confirm("Are you sure?");
+    const toConfirm = confirm("Are you sure?");
     if (!toConfirm) {
         return;
     }
-    else if(toConfirm) {
-        clearInterval(countDown);
-        countDown = null;
-        secs = 60;
-        displayTime.innerHTML = secs;
+    else if (toConfirm) {
+        clockResetting();
         currScore = 0;
         isClicked.fill(false);
         hideGame();
         document.querySelector('.start').innerHTML = "Start"
     }
-    //lets add one option to reset the scorecards
 }
